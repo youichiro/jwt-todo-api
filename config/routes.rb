29541b04b390b2f4abd66add7ef87fe360967d29
youichiro <cinnamon416@gmail.com>
2020-06-29 06:49:56 +0900

    [add] tasksアクションのroutesを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope :api do
+    resources :tasks, only: [:index, :show, :creare, :update, :destroy]
+  end
 end
