85e8d1ec3c92c7fbeb7be5c98dd61f10c5419426
youichiro <cinnamon416@gmail.com>
2020-06-29 06:37:48 +0900

    [update] taskモデルのmigrate

--- db/migrate/20200628213620_create_tasks.rb
+++ db/migrate/20200628213620_create_tasks.rb
@@ -2,8 +2,8 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :title
-      t.boolean :done
+      t.string :title, null: false, limit: 100
+      t.boolean :done, null: false, default: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_213325) do
+ActiveRecord::Schema.define(version: 2020_06_28_213620) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", limit: 100, null: false
+    t.boolean "done", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", limit: 100, null: false
@@ -21,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_06_28_213325) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
