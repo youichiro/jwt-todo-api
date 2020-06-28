80dffd76deea72079a7cb20962a930065a153760
youichiro <cinnamon416@gmail.com>
2020-06-29 06:53:48 +0900

    [add] jwtトークンを生成するアクションを作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+  include JwtAuthenticator
+
+  def create
+    current_user = User.find_by(email: params[:email])
+    if current_user&.authenticate(params[:password])
+      jwt_token = encode(current_user.id)
+      response.headers['X-Authentication-Token'] = jwt_token
+      render json: current_user
+    else
+      render json: { message: 'unauthorized'}, status: 401
+    end
+  end
 end
