69565858ab557394f70671ec0a8680a1889a7b10
youichiro <cinnamon416@gmail.com>
2020-06-29 07:37:31 +0900

    [fix] typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   scope :api do
     resources :sessions, only: [:create]
     resources :users, only: [:create, :show]
-    resources :tasks, only: [:index, :show, :creare, :update, :destroy]
+    resources :tasks, only: [:index, :show, :create, :update, :destroy]
   end
 end
