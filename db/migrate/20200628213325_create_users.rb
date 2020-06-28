78d80a76ac594d849e9ad56da107a7080cd641b1
youichiro <cinnamon416@gmail.com>
2020-06-29 06:35:52 +0900

    [update] userモデルのmigrate

--- db/migrate/20200628213325_create_users.rb
+++ db/migrate/20200628213325_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
+      t.string :name, null: false, limit: 100
+      t.string :email, null: false, limit: 100
+      t.string :password_digest, null: false
 
       t.timestamps
+      t.index :email, unique: true
     end
   end
 end
