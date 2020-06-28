c811f836f2089d7aef0bc4f6052e6d45dc4d35b0
youichiro <cinnamon416@gmail.com>
2020-06-29 07:00:01 +0900

    [add] JWT承認してユーザを取得するアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  include JwtAuthenticator
+  before_action :jwt_authenticate, except: :create
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors
+    end
+  end
+
+  def show
+    render json: @current_user
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
 end
