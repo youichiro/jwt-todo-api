41c54264eead74d31d862866d8b18902c12f93a4
youichiro <cinnamon416@gmail.com>
2020-06-29 07:26:34 +0900

    [add] routesの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   scope :api do
+    resources :sessions, only: [:create]
+    resources :users, only: [:create, :show]
     resources :tasks, only: [:index, :show, :creare, :update, :destroy]
   end
 end
