09b496b067e70ff45ed5eb1b245c6a893d7849bb
youichiro <cinnamon416@gmail.com>
2020-06-29 06:41:32 +0900

    [update] userモデルのmodels定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :tasks, dependent: :destroy
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :email, presence: true, length: { maximum: 100 }, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
+  before_save :downcase_email
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
